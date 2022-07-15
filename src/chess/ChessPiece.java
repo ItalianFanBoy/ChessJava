6471e06d01060224d255305ce1bcdffdd424353f
Guilherme Amaral <amaral.guilherme@hotmail.com>
2022-07-15 12:38:32 -0300

    Migrando para vs code
    migrando para vs code

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,27 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+
+public abstract class ChessPiece extends Piece {
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	protected boolean isThereOpponentPiece(Position positon) {
+		ChessPiece p = (ChessPiece)getBoard().piece(positon);
+		return p != null && p.getColor() != color;
+	}
+
+	protected abstract ChessPosition getChessPosition();
+	
+	
+}
